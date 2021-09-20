cf5ace780c24eb2a5043df10596ea402a5f1218a
Antonio Mesquita <antonio.mesquita@ifood.com.br>
2021-09-19 22:00:06 -0300

    fix: remove junk

--- teste.c
+++ teste.c
@@ -12,7 +12,7 @@ int main(void) {
         fputs("erro ao abrir arquivo", stderr);
         exit(1);
     }
-    
+
 	FILE* utf8out = fopen("peq_utf_out.txt", "wb");
     if (utf8out == NULL) {
         fputs ("erro ao abrir arquivo",stderr);
@@ -20,26 +20,6 @@ int main(void) {
     }
 
     utf_varint(utf8, utf8out);
-
-
-
-	// FILE* utf32 = fopen("utf32_peq.txt", "rb");
-    // if (utf32 == NULL) {
-    //     fputs ("erro ao abrir arquivo",stderr);
-    //     exit (1);
-    // }
-	// FILE* utf32out = fopen("utf32_peq_out.txt", "wb");
-    // if (utf32out == NULL)
-    //     fputs("erro ao abrir arquivo",stderr); exit (1);
-	
-
-	// printf("O processador deste computador e: %s Endian.\n", isLittleEndian() ? "Little" : "Big");
-	
-	// utf8_32(utf8, utf32out);
-	// utf32_8(utf32, utf8out);
-	
-	// fclose(utf8);
-	// fclose(utf32);
 
     return 1;
 }
